5895431c6f19e028e152c3745c83ff1ca93e8e0d
Al-amin Sheikh <azalamins@gmail.com>
2022-05-19 07:00:19 +0600

    added completed task api

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
-require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const app = express();
 
 app.use(cors());
@@ -19,11 +19,31 @@ async function run() {
   try {
     await client.connect();
     const taskCollection = client.db("todoApp").collection("task");
-    console.log('db connected');
+    console.log("db connected");
+
+    app.get("/task", async (req, res) => {
+      const result = await taskCollection.find().toArray();
+      res.send(result);
+    });
+
     app.post("/task", async (req, res) => {
       const task = req.body;
-        const result = await taskCollection.insertOne(task);
-        res.send(result);
+      const result = await taskCollection.insertOne(task);
+      res.send(result);
+    });
+
+    app.patch("/task/:id", async (req, res) => {
+      const id = req.params.id;
+      const { isCompleted } = req.body;
+      const query = { _id: ObjectId(id) };
+      console.log(isCompleted);
+      const updatedDoc = {
+        $set: {
+          isCompleted: true,
+        },
+      };
+      const result = await taskCollection.updateOne(query, updatedDoc);
+      res.send(result);
     });
   } finally {
     // await client.close();
